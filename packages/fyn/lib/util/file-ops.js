6593cfff3f431c3e86185cdb1ad6259a9d9f1f92
Joel Chen <joel123@gmail.com>
2025-11-01 21:31:49 -0700

    Replace rimraf with fs.promises.rm and bump Node.js requirement to 14.14+

--- packages/fyn/lib/util/file-ops.js
+++ packages/fyn/lib/util/file-ops.js
@@ -4,10 +4,17 @@ const Promise = require("./aveazul");
 const opfs = require("opfs");
 const lockfile = require("lockfile");
 const win32Opfs = require("./file-ops-win32");
+const fs = require("fs");
 
 opfs._opfsSetPromise(Promise);
 
 opfs.$.acquireLock = Promise.promisify(lockfile.lock, { context: lockfile });
 opfs.$.releaseLock = Promise.promisify(lockfile.unlock, { context: lockfile });
+
+// Add rimraf implementation using fs.rm (Node.js 14.14+)
+// Replace the opfs rimraf wrapper with native fs.rm
+opfs.$.rimraf = async (path) => {
+  return fs.promises.rm(path, { recursive: true, force: true });
+};
 
 module.exports = win32Opfs(opfs);
